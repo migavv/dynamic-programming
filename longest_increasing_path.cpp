4ef32e189ac3d0c483071a845272d37ff7257477
migavv <migaviria@outlook.com>
2025-06-23 23:26:44 -0500

    longest increasing path

--- longest_increasing_path.cpp
+++ longest_increasing_path.cpp
@@ -1,35 +1,40 @@
+#include <cstring>
 #include <iostream>
 
 using namespace std;
 
+int n;
+int matrix[100][100];
+int dp[100][100];
+
+int dfs(int i, int j) {
+    if (dp[i][j] != -1) { return dp[i][j]; }
+    int res = 1;
+    if (i - 1 >= 1 && matrix[i-1][j] - matrix[i][j] == 1) {
+        res = max(res, dfs(i-1, j) + 1);
+    } else if (i + 1 <= n && matrix[i+1][j] - matrix[i][j] == 1) {
+        res = max(res, dfs(i+1, j) + 1);
+    } else if (j - 1 >= 1 && matrix[i][j-1] - matrix[i][j] == 1) {
+        res = max(res, dfs(i, j-1) + 1);
+    } else if (j + 1 <= n && matrix[i][j+1] - matrix[i][j] == 1) {
+        res = max(res, dfs(i, j+1) + 1);
+    }
+    return dp[i][j] = res;
+}
+
 int main() {
-    int n; cin >> n;
-    int matrix[100][100];
-    int dp[100][100][100] = {};
+    cin >> n;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             cin >> matrix[i][j];
         }
     }
+    memset(dp, -1, sizeof(dp));
+    int res = 1;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            for (int k = 1; k <= n; k++) {
-                if (j-1 >= 0 && matrix[i][j] < matrix[i][j-1]) {
-                  dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][k-1]);
-                } else if (j+1 <= n && matrix[i][j] < matrix[i][j+1]) {
-                    dp[i][j][k] = max(dp[i][j][k], dp[i][j+1][k-1]);
-                } else if (i-1 >= 0 && matrix[i][j] < matrix[i-1][j]) {
-                    dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k-1]);
-                } else if (i+1 <= n && matrix[i][j] < matrix[i+1][j]) {
-                    dp[i][j][k] = max(dp[i][j][k], dp[i+1][j][k-1]);
-                }
-            }
+            res = max(res, dfs(i, j));
         }
     }
-    int res = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            res = max(res, dp[i][j][])
-        }
-    }
+    cout << res << endl;
 }
